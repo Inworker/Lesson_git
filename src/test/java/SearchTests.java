6a45e67680fec8c2f762c69271ee41832be00dd6
Inworker <31877506+Inworker@users.noreply.github.com>
2025-10-01 22:06:44 +0300

    Update SearchTests.java

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -2,7 +2,7 @@ public class SearchTests {
     @Test
     void successfulSearchTest() {
         open("https://www.google.com/");
-        $("[name=q]").setValue("selenide").pressEnter();
+        $("[name=q]").setValue("selenide!!!!!!!!!!!!!").pressEnter();
         $("[id=search]").shouldHave(text("https://rambler.org"));
     }
 }
